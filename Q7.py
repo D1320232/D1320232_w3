862cb709a87bc83b9955755aa0b068864ee87d9d
D1320232 <D1320232@o365.fcu.edu.tw>
2024-09-26 11:34:06 +0800

    Q8.py

--- Q7.py
+++ Q7.py
@@ -3,9 +3,6 @@ decimal_number=d
 b=bin(d)
 c=oct(d)
 e=hex(d)
-print(b)
-print(c)
-print(e)
 print('{:08b}'.format(d,'b')) #b=二進位
 print('{:03o}'.format(d,'o')) #o=八進位
 print('{:03x}'.format(d,'x'))
